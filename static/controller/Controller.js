021450e4f836cdf98a7b2d4ec16b5108d139f5aa
rkwhitlock <reesekwhitlock@gmail.com>
2023-11-04 13:08:24 -0700

    connect /api/connect/ to button

--- static/controller/Controller.js
+++ static/controller/Controller.js
@@ -1,24 +1,6 @@
 const testButtonClicked = () => {
-  $.ajax({
-    url: "/api/connect",
-    type: "POST",
-    contentType: "application/json;charset=utf-8",
-    dataType: "json",
-    success: function (response) {
-      console.log("?");
-      if (response.success) {
-        var responseText = document.getElementById("responseText");
-        console.log("Success");
-        console.log(response);
-        responseText.innerText = response.text;
-        responseText.style.display = "block";
-      } else {
-        console.log("Failed to get text: " + response.message);
-      }
-    },
-    error: function (error) {
-      console.log("/");
-      console.log(error);
-    },
-  });
+  console.log("?");
+  var request = new XMLHttpRequest();
+  request.open("POST", "/api/connect", true);
+  request.send();
 };
--- static/view/index.js
+++ static/view/index.js
@@ -2,15 +2,15 @@ body = () => {
   const style = document.createElement("link");
   style.href = "static/styles.css";
 
-  const main = document.getElementById("main");
+  const main = document.getElementById("responseText");
   const centerContainer = document.createElement("div");
   const label = document.createElement("h1");
 
   label.textContent = "Food Insecurity Model";
 
   const testButton = document.createElement("button");
-  testButton.onclick = testButtonClicked();
-  testButton.textContent("Click");
+  testButton.onclick = testButtonClicked;
+  testButton.textContent = "Click";
 
   centerContainer.append(label, testButton);
   main.append(centerContainer);
