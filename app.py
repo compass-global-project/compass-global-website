e507d43c2437855552f81b8569ebc83a432597c5
rkwhitlock <reesekwhitlock@gmail.com>
2023-11-11 13:59:19 -0800

    india data and geojson

--- app.py
+++ app.py
@@ -17,23 +17,27 @@ api_key = "QY3IWPTMoJu2gcDIYM81oweqzSTlomXd"
 
 headers = {"Content-Type": "application/json", "Authorization": ("Bearer " + api_key)}
 
+df = pd.read_csv("data.csv")
+import geojson
+
+
+with open("india.geojson") as f:
+    gj = geojson.load(f)
+
+fig = px.choropleth(
+    df,
+    geojson=gj,
+    featureidkey="properties.ST_NM",
+    locations="state",
+    color="active cases",
+    color_continuous_scale="Reds",
+)
+
+fig.update_geos(fitbounds="locations", visible=False)
+
 
 @app.route("/heatmap")
 def heat_map():
-    df = pd.read_csv(
-        "https://gist.githubusercontent.com/jbrobst/56c13bbbf9d97d187fea01ca62ea5112/raw/e388c4cae20aa53cb5090210a42ebb9b765c0a36/active_cases_2020-07-17_0800.csv"
-    )
-
-    fig = px.choropleth(
-        df,
-        geojson="https://gist.githubusercontent.com/jbrobst/56c13bbbf9d97d187fea01ca62ea5112/raw/e388c4cae20aa53cb5090210a42ebb9b765c0a36/india_states.geojson",
-        featureidkey="properties.ST_NM",
-        locations="state",
-        color="active cases",
-        color_continuous_scale="Reds",
-    )
-
-    fig.update_geos(fitbounds="locations", visible=False)
     fig.show()
 
 
